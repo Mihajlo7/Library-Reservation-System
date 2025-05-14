c53c31ad6302f92cb0060b0519b2c2dd03482381
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2025-05-14 19:38:02 +0200

    Created project for Entities. Created Book Entity

---
+++ LibrarySystem.Entities/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Entities.Models
+{
+    public class Book : IEntity
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublishedYear { get; set; }
+        public string Isbn { get; set; }
+    }
+}
